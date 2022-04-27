628c3950f656e877aa0c9674751333e609b65b9a
aztlanleuc <dm.phone766@gmail.com>
2022-04-27 13:34:50 +1000

    Modify german verb script to also create definition flashcards

--- german-verb-generator/script.py
+++ german-verb-generator/script.py
@@ -6,11 +6,15 @@ import csv
 input_file = open("input-verbs.tsv")
 input_content = csv.reader(input_file, delimiter="\t")
 
-# the output file will have two columns written to it, the verb infinitive with
-# a pronoun in brackets, and the conjugated form
-output_file = open("output-cards.tsv", "wt")
+# we want to have two output files: one for conjugation cards and one for definition cards
+# the conjugation file will have two columns, first the verb infinitive with a pronoun in brackets,
+# followed by the conjugation of the verb for that pronoun. the definition file will have
+# two columns as well, first the verb infinitive followed by the english translation
+conjugation_file = open("conjugation-cards.tsv", "wt")
+definition_file = open("definition-cards.tsv", "wt")
 
-tsv_writer = csv.writer(output_file, delimiter="\t")
+conj_writer = csv.writer(conjugation_file, delimiter="\t")
+def_writer = csv.writer(definition_file, delimiter="\t")
 
 # keep track of the number of lines we've processed for funsies
 # TODO: time how long it takes to process?
@@ -23,16 +27,19 @@ for line in input_content:
         line_count += 1
         continue
 
-    # extract out the word and conjugations, and write them to the output file
+    # extract out the word and conjugations, and write them to the conjugation file
     word = line[0]
     print(word)
-    tsv_writer.writerow([word + " (ich)", line[1]])
-    tsv_writer.writerow([word + " (du)", line[2]])
-    tsv_writer.writerow([word + " (er/sie/es)", line[3]])
-    tsv_writer.writerow([word + " (wir)", line[4]])
-    tsv_writer.writerow([word + " (ihr)", line[5]])
-    tsv_writer.writerow([word + " (sie)", line[6]])
-    tsv_writer.writerow([word + " (Sie)", line[7]])
+    conj_writer.writerow([word + " (ich)", line[2]])
+    conj_writer.writerow([word + " (du)", line[3]])
+    conj_writer.writerow([word + " (er/sie/es)", line[4]])
+    conj_writer.writerow([word + " (wir)", line[5]])
+    conj_writer.writerow([word + " (ihr)", line[6]])
+    conj_writer.writerow([word + " (sie)", line[7]])
+    conj_writer.writerow([word + " (Sie)", line[8]])
+
+    # write the word and defintion to the definition
+    def_writer.writerow([word, line[1]])
 
     line_count += 1
 
@@ -40,5 +47,7 @@ for line in input_content:
 # since there's a header row
 print(f'processed {line_count - 1} verbs')
 
+# close all the files
 input_file.close()
-output_file.close()
+conjugation_file.close()
+definition_file.close()
